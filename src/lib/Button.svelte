9835a85b0940bb9a01d964a8c7e3802ef6cffb83
Samuel Amaro <samuelamaro96746313@gmail.com>
2025-03-20 21:45:48 -0300

    feat: add component `Button`

---
+++ src/lib/Button.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import type { HTMLButtonAttributes } from 'svelte/elements';
+
+	interface Props extends HTMLButtonAttributes {
+		variant: 'primary' | 'secondary';
+	}
+
+	let { children, variant, ...rest }: Props = $props();
+</script>
+
+<button
+	{...rest}
+	class={[
+		rest.class,
+		'box-border flex h-[48px] w-full appearance-none items-center justify-center rounded-[5px] border-none text-base font-bold outline-none',
+		variant === 'primary' && 'bg-violet hover:bg-light-violet focus:bg-light-violet text-white',
+		variant === 'secondary' &&
+			'bg-custom-1 dark:bg-custom-2 text-violet hover:bg-custom-3 focus:bg-custom-3 dark:hover:bg-custom-4 focus:hover:bg-custom-4 mix-blend-multiply dark:text-white'
+	]}
+>
+	{@render children?.()}
+</button>
